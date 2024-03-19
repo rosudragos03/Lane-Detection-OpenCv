96f9dceb5293e31b0a9994fa0f34cf0798859f81
rosudragos03 <03rosu@gmail.com>
2024-03-19 16:51:05 +0200

    detectia linilor albe

--- Software/Edge_Detection/main.py
+++ Software/Edge_Detection/main.py
@@ -17,20 +17,27 @@ while True:
         print("Nu pot primii frameuri (video terminat?). Sfarsit...")
         break
 
-    # Redimensionare la noua rezolutie
+    # noua rezolutie
     frame = cv.resize(frame, (new_width, new_height))
 
     # Setează partea de sus a imaginii la culoarea neagră
     frame[:new_height//2, :] = [0, 0, 0]
 
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)  # setare in nuante de gri
-    edges = cv.Canny(gray, 300, 400)  # creare contururi
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY) # imagine alb-negru
 
-    # Afisare frameuri origibal si final
-    cv.imshow('Original Video', frame)
+    edges = cv.Canny(gray, 300, 400) # creare contururi
+    # detectarea linilor albe
+    lines = cv.HoughLinesP(edges, 1, np.pi / 180, threshold=100, minLineLength=100, maxLineGap=50)
+
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line[0]
+            cv.line(frame, (x1, y1), (x2, y2), (0, 0, 255), 2) #identificarea linilor albe
+
     cv.imshow('Edge Detection', edges)
+    cv.imshow('Original Video', frame)  # afisare video result
 
-    if cv.waitKey(1) == ord('q'):  # mod de incheiere al videourilor/programului
+    if cv.waitKey(1) == ord('q'):  # mod de încheiere al videourilor/programului
         break
 
 cap.release()  # eliberare variabila
